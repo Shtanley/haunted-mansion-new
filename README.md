8946139cc6ca80afed5977b7ba211fc9e720cd04
Sameer Hossain <44882802+Shtanley@users.noreply.github.com>
2023-07-22 12:19:13 -0700

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,99 @@
+ 
+
+
+> Open this page at [https://shtanley.github.io/maze_game_anim/](https://shtanley.github.io/maze_game_anim/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/shtanley/maze_game_anim** and import
+
+## Edit this project ![Build status badge](https://github.com/shtanley/maze_game_anim/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/shtanley/maze_game_anim** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/shtanley/maze_game_anim/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Open this page at [https://shtanley.github.io/haunted-mansion/](https://shtanley.github.io/haunted-mansion/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/shtanley/haunted-mansion** and import
+
+## Edit this project ![Build status badge](https://github.com/shtanley/haunted-mansion/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/shtanley/haunted-mansion** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/shtanley/haunted-mansion/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Open this page at [https://shtanley.github.io/haunted-mansion-new/](https://shtanley.github.io/haunted-mansion-new/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/shtanley/haunted-mansion-new** and import
+
+## Edit this project ![Build status badge](https://github.com/shtanley/haunted-mansion-new/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/shtanley/haunted-mansion-new** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/shtanley/haunted-mansion-new/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ tilemap.g.ts
@@ -0,0 +1,179 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile20 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile26 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile23 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile19 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile21 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile27 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile24 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile25 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile28 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile29 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile30 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile22 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile31 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`200020000808020908020806070902020809020808080209080208020209020208090208090f1313131313161613131313131009090f1313131313131313131313131009080e0101010101010101010101010c02080e0101010101010101010101010c02140e0101010101010101010101010c0b150e0101011c011c011c010101010c14090e0101010101010101010101010c02090e011d1817171717171a1b01010c02080e0101010101010101010101010c09080e0101011901190119010101010c0902110101010101010101010101010c08020e01010101010101010101010112080b01010101010101010101010101010a0a01010101010101010101010101010b0b01010101010101010101010101010d0d01010101010101010101010101010b020f0101010101010101010101010c09020e0101010101010101010101011009020e0101010101010101010101010c08020e0101011c011c011c010101010c08090e0101010101010101010101010c09090e011d1817171717171a1b01010c09150e0101010101010101010101010c150b0e0101011901190119010101010c0b090e0101010101010101010101010c09090e0101010101010101010101010c0908110d0d0d0d0d01010d0d0d0d0d120908110d0d0d0d0d01010d0d0d0d0d1209090208080902021e1e02080902090202090208080902020304020809020902020808020908020806070902020809020808080209080208060709020208090208090f1313131323010113131313131009090f1313131313010113131313131009080e0101010101010101010101010c02080e0101010101010101010101010c020b0e0101010101010101010101010c150b0e0101010101010101010101010c15090e0101010101010101010101010c02090e0101010101010101010101010c02080e0101010101010101010101010c09080e0101010101010101010101010c0902110101010101010101010101010c08020e01010101010101010101010112080b01010101010101010101010101010a0a01010101010101010101010101010b0b0101010101010101010101010101050501010101010101010101010101010b020f0101010101010101010101010c09020e0101010101010101010101011009020e0101010101010101010101010c08020e0101010101010101010101010c08090e0101010101212201010101010c09090e0101010101010101010101010c09140e01010101011f2001010101010c0b150e0101010101010101010101010c14090e0101010101010101010101010c09090e0101010101010101010101010c0908110d0d0d0d0d0d0d0d0d0d0d0d120908110d0d0d0d0d0d0d0d0d0d0d0d1209090208080902020b1502080902090202090208080902020b1502080902090202`, img`
+2222222..22222222222222222222222
+2..............22..............2
+2..............22..............2
+................2...............
+2..............22...2222222....2
+2..............22..............2
+2..............22..............2
+................................
+................................
+2..............22..............2
+2..............22..............2
+2..............22...2222222....2
+2..............2................
+2..............22..............2
+2..............22..............2
+2222222..22222222222222..2222222
+2222222..22222222222222..2222222
+2..............22..............2
+2..............22..............2
+...............2...............2
+2..............22..............2
+2..............22..............2
+2..............22..............2
+................................
+................................
+2..............22..............2
+2..............22..............2
+2..............22..............2
+................2...............
+2..............22..............2
+2..............22..............2
+2222222.222222222222222.22222222
+`, [myTiles.transparency16,sprites.dungeon.darkGroundCenter,myTiles.tile3,myTiles.tile6,myTiles.tile7,myTiles.tile9,myTiles.tile15,myTiles.tile16,myTiles.tile1,myTiles.tile2,myTiles.tile10,myTiles.tile5,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundNorth,sprites.dungeon.collectibleInsignia,sprites.dungeon.floorLight3,myTiles.tile4,myTiles.tile18,myTiles.tile20,myTiles.tile23,myTiles.tile26,myTiles.tile19,myTiles.tile21,myTiles.tile27,sprites.dungeon.stairNorth,myTiles.tile24,myTiles.tile25,myTiles.tile28,myTiles.tile29,myTiles.tile22,myTiles.tile31], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile13":
+            case "tile15":return tile15;
+            case "myTile14":
+            case "tile16":return tile16;
+            case "myTile15":
+            case "tile17":return tile17;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile11":
+            case "tile13":return tile13;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile12":
+            case "tile14":return tile14;
+            case "myTile16":
+            case "tile18":return tile18;
+            case "myTile18":
+            case "tile20":return tile20;
+            case "myTile24":
+            case "tile26":return tile26;
+            case "myTile21":
+            case "tile23":return tile23;
+            case "myTile17":
+            case "tile19":return tile19;
+            case "myTile19":
+            case "tile21":return tile21;
+            case "myTile25":
+            case "tile27":return tile27;
+            case "myTile22":
+            case "tile24":return tile24;
+            case "myTile23":
+            case "tile25":return tile25;
+            case "myTile26":
+            case "tile28":return tile28;
+            case "myTile27":
+            case "tile29":return tile29;
+            case "myTile28":
+            case "tile30":return tile30;
+            case "myTile20":
+            case "tile22":return tile22;
+            case "myTile29":
+            case "tile31":return tile31;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
